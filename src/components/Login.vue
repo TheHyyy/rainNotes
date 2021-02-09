e17346f037da30ffcf78428ddfd3b2623e8744ef
Hyyy_1 <447542807@qq.com>
2021-02-09 23:42:19 +0800

    update

---
+++ src/components/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h1>是我吗 兄弟！</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {}
+};
+</script>
+
+<style scoped>
+* {
+  color: #66ccff;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,35 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import Login from "@/components/Login";
+import NotebookList from "@/components/NotebookList";
+import Note from "@/components/NoteDetail";
+import Trash from "@/components/TrashDetail";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "HelloWorld",
       component: HelloWorld
+    },
+    {
+      path: "/login",
+      component: Login
+    },
+    {
+      path: "/notebooks",
+      component: NotebookList
+    },
+    {
+      path: "/note/:noteId",
+      component: Note
+    },
+    {
+      path: "/trash/:noteId",
+      component: Trash
     }
   ]
-})
+});
